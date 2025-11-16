bb8b2c73577f51b9e88d83ab5fe835916e2651bc
NID <nid@localhost.localdomain>
2025-11-16 17:51:57 +0100

    [ADD] : GITHUB FUN PUBLIC REPOSITORY ADDED

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ cp .env dist/
 cd dist/
 git init
 git add .
-git commit -m "[DONE] : Browser Compatibility"
+git commit -m "[DONE] : GITHUB FUN PUBLIC CODE ADDED"
 git branch -M main
 git remote add origin git@github.com:IbrahimDiaby/CV.git
 git push -u origin main
