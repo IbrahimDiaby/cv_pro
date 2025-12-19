17bb885358b6ea52ce2066da68bf28e8b4ea4d4c
NID <nid@localhost.localdomain>
2025-12-19 23:53:02 +0100

    [UPDATE] : Frontend Mentor challenge added

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -37,6 +37,24 @@ const Projects = () => {
   const items: Array<ProjectInterface> = useMemo<Array<ProjectInterface>>(
     () => [
       {
+        image: "social-links-profile.png",
+        title: "Social Links Profile",
+        role: "Développeur Web Fullstack",
+        description: `Petit défi d'implémentation de maquette sans framework. Que du pure code "Vanilla" Test de compétence réelle. Challenge Frontend Mentor`,
+        technologies: [
+          "HTML",
+          "CSS",
+          // "JS",
+          "GIT",
+          "GITHUB (Pages)",
+        ],
+        url: "https://ibrahimdiaby.github.io/social-links-profile-main",
+        tools: [],
+        type: "github",
+        start: "2025",
+        end: "2025",
+      },
+      {
         image: "sygesco.png",
         title: "SYGESCO",
         role: "Développeur Web Fullstack",
