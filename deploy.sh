b118767c8369f117ad45ca71050bca10c43f709d
Ibrahim Diaby <d.ibrahim2016@gmail.com>
2026-01-05 00:58:18 +0100

    [REFACTORED] : Script updated

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
-npm run build
-cp .env dist/
-cd dist/
-git init
-git add .
-git commit -m "[DONE] : 2026 Started"
-git branch -M main
-git remote add origin git@github.com:IbrahimDiaby/CV.git
-git push -u origin main
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: ./deploy.sh FLAG MESSAGE"
+    echo "git commit -m \"[FLAG] : [MESSAGE]\""
+    exit 0
+else
+    npm run build
+    cp .env dist/
+    cd dist/
+    git init
+    git add .
+    git commit -m "[$1] : $2"
+    git branch -M main
+    git remote add origin git@github.com:IbrahimDiaby/CV.git
+    git push -u origin main
+fi
