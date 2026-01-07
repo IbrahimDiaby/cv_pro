51e4da71e7a73ab7317bbcb977e51ae125a0df75
Ibrahim Diaby <d.ibrahim2016@gmail.com>
2026-01-07 15:58:40 +0100

    [ADD] : Observer and Font added

---
+++ src/utils/observer.ts
@@ -0,0 +1,24 @@
+const options = {
+    rootMargin: "0px 0px",
+    root: document.querySelector("#root"),
+    scrollMargin: "0px",
+    threshold: 0
+}
+
+export const myObserver = {
+    className : "visible",
+    observer : new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add(myObserver.className);
+            // myObserver.observer.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove(myObserver.className);
+            // myObserver.observer.unobserve(entry.target);
+        }
+        return;
+
+    })}, options)
+
+}
+
